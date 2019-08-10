361858c8e512dbc368548fd362ac48760d02abfd
kaushalkumar22 <kaushalkumar379@gmail.com>
2019-08-10 21:55:36 +0530

    adding

--- src/com/ds/problemset/BinaryTreeCameras.java
+++ src/com/ds/problemset/BinaryTreeCameras.java
@@ -3,26 +3,26 @@ package com.ds.problemset;
 import com.ds.binarytree.TreeNode;
 
 /**
- * Given a binary tree, we install cameras on the nodes of the tree. 
+Given a binary tree, we install cameras on the nodes of the tree. 
 Each camera at a node can monitor its parent, itself, and its immediate children.
 Calculate the minimum number of cameras needed to monitor all nodes of the tree.
- 
+
 Example 1:
- 
+
 Input: [0,0,null,0,0]
 Output: 1
 Explanation: One camera is enough to monitor all nodes if placed as shown.
 Example 2:
- 
+
 Input: [0,0,null,0,null,0,null,null,0]
 Output: 2
-Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
- * @author IBM
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of
+ the valid configurations of camera placement.
  *
  */
 public class BinaryTreeCameras {
 
-/*Consider a node in the tree.
+	/*Consider a node in the tree.
 It can be covered by its parent, itself, its two children.
 Four options.
 
@@ -58,20 +58,21 @@ if it has a child, which is the parent of a leaf (node 1), then it's covered.
 If it needs camera, then res++ and we return 1.
 If it's covered, we return 2.
 Otherwise, we return 0.
-*/
+	 */
 
-    int res = 0;
-    public int minCameraCover(TreeNode root) {
-        return (dfs(root) < 1 ? 1 : 0) + res;
-    }
+	int res = 0;
+	public int minCameraCover(TreeNode root) {
+		return (dfs(root) < 1 ? 1 : 0) + res;
+	}
 
-    public int dfs(TreeNode root) {
-        if (root == null) return 2;
-        int left = dfs(root.left), right = dfs(root.right);
-        if (left == 0 || right == 0) {
-            res++;
-            return 1;
-        }
-        return left == 1 || right == 1 ? 2 : 0;
-    }
+	public int dfs(TreeNode root) {
+		if (root == null) return 2;
+		int left = dfs(root.left);
+		int right = dfs(root.right);
+		if (left == 0 || right == 0) {
+			res++;
+			return 1;
+		}
+		return left == 1 || right == 1 ? 2 : 0;
+	}
 }
