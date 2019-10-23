696cd7598d680e20e0cf97993d2c0019e2e1a5cd
Kumar <kaushal.kumar@sap.com>
2019-10-23 11:26:29 +0530

    misc

---
+++ src/com/ds/miscellaneous/WordSubsets.java
@@ -0,0 +1,59 @@
+package com.ds.miscellaneous;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+We are given two arrays A and B of words.  Each word is a string of lowercase letters.
+Now, say that word b is a subset of word a if every letter in b occurs in a, including multiplicity.  For example, "wrr" is a subset of "warrior", but is not a subset of "world".
+Now say a word a from A is universal if for every b in B, b is a subset of a. 
+Return a list of all universal words in A.  You can return the words in any order.
+
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","o"]
+Output: ["facebook","google","leetcode"]
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["l","e"]
+Output: ["apple","google","leetcode"]
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["e","oo"]
+Output: ["facebook","google"]
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["lo","eo"]
+Output: ["google","leetcode"]
+Input: A = ["amazon","apple","facebook","google","leetcode"], B = ["ec","oc","ceo"]
+Output: ["facebook","leetcode"]
+
+ * @author IBM
+ *
+ */
+public class WordSubsets {
+	
+	public static void main(String[] args) {
+		String[] A = {"amazon","apple","facebook","google","leetcode"};
+		String[] B = {"e","o"};
+		System.out.println(wordSubsets(A,B));
+	}
+
+	 public static  List<String> wordSubsets(String[] A, String[] B) {
+			
+			List<String> res = new ArrayList<>();
+			int[] count = new int[26];
+			for (String b : B) {
+				int[]   tmp = counter(b);
+	            for (int i = 0; i < 26; ++i)
+	                count[i] = Math.max(count[i], tmp[i]);
+	        }
+			for (String a : A) {
+				int[]  tmp = counter(a);
+				for ( int i = 0; i < 26; i++){
+					if (count[i]>tmp[i] )
+						break;
+					if (i == 25) res.add(a);
+				}
+			}
+			return res;
+		}
+
+		static  int[] counter(String word) {
+			int[] count = new int[26];
+			for (char c : word.toCharArray()) count[c - 'a']++;
+			return count;
+		}
+}
--- src/com/ds/universal/Histogram.java
+++ src/com/ds/universal/Histogram.java
@@ -64,44 +64,4 @@ public class Histogram {
 		return maxArea;
 
 	}
-	/*
-		Stack<Integer> s = new Stack<Integer>();
-
-		int hMaxArea = 0;
-		int top;  // To store top of stack
-		int areaWithTop; // To store area with top bar as the smallest bar
-		//Start from first bar, and do following for every bar ‘hArray[i]’ where ‘i’ varies from 0 to n-1.
-		int i = 0;
-		while (i < n){
-			// If stack is empty or hArray[i] is higher than hArray[s.peek()], then push ‘i’ to stack.
-			if (s.empty() ||hArray[s.peek()] <= hArray[i]){
-				s.push(i);
-				i++;
-
-				//if hArray[i] is smaller than top of stack hArray[s.peek()], then keep removing the top of stack while top of the stack is greater
-				// Let the removed bar be hist[tp]. Calculate area of rectangle with hArray[top] as smallest bar. For hArray[top], the ‘left index’ is previous 
-				//(previous to top) item in stack and ‘right index’ is ‘i’ (current index). 
-			}else{    	
-				top = s.peek();  
-				s.pop(); 
-				// Calculate the area with hist[top] stack as smallest bar
-				areaWithTop = hArray[top] * (s.empty() ? i : i - s.peek() - 1);
-
-				if (hMaxArea < areaWithTop){
-					hMaxArea = areaWithTop;
-				}
-			}
-		}
-		// Now pop the remaining bars from stack and calculate area with every
-		while (s.empty() == false){
-			top = s.peek();
-			s.pop();
-			areaWithTop = hArray[top] * (s.empty() ? i : i - s.peek() - 1);
-
-			if (hMaxArea < areaWithTop){
-				hMaxArea = areaWithTop;
-			}
-		}
-		System.out.println(hMaxArea);
-	}*/
 }
